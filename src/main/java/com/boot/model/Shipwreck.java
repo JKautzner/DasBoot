85b5454f0d22c4cd1c72e06a6c95c22f79b487f6
John Kautzner <john.kautzner@daugherty.com>
2017-01-20 15:42:20 -0600

    JPA Repo

--- src/main/java/com/boot/model/Shipwreck.java
+++ src/main/java/com/boot/model/Shipwreck.java
@@ -1,6 +1,15 @@
 package com.boot.model;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+//  JPA Entity  (Something that goes in the database)
+@Entity
 public class Shipwreck {
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
 	Long id;
 	String name;
 	String description;
